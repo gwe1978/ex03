f75ffe7c6be2856d30cb5b7b44639f1ed7af3655
gwe1978 <guilhem.wetteren@gmail.com>
2018-10-26 09:45:30 +0200

    [Classe Client] Ajout des "getters"

--- src/Client.java
+++ src/Client.java
@@ -1,10 +1,18 @@
 
 public class Client {
-	String nom;
-	String prenom;
+	private String nom;
+	private String prenom;
 	
 	public Client(String nom, String prenom) {
 		this.nom = nom;
 		this.prenom = prenom;
+	}
+	
+	public String getNom() {
+		return this.nom;
+	}
+	
+	public String getPrenom() {
+		return this.prenom;
 	}
 }
